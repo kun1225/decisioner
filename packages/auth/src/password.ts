bc52a1e0ba9eee38468df4d2b8658da71d80dda9
kun1225 <kun881225@gmail.com>
2026-02-10 14:34:48 +0800

    chore(format): enable semicolons in auth package

--- packages/auth/src/password.ts
+++ packages/auth/src/password.ts
@@ -1,14 +1,14 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 
-const SALT_ROUNDS = 12
+const SALT_ROUNDS = 12;
 
 export async function hashPassword(password: string): Promise<string> {
-  return bcrypt.hash(password, SALT_ROUNDS)
+  return bcrypt.hash(password, SALT_ROUNDS);
 }
 
 export async function verifyPassword(
   password: string,
   hash: string,
 ): Promise<boolean> {
-  return bcrypt.compare(password, hash)
+  return bcrypt.compare(password, hash);
 }
