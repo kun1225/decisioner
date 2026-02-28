863386c4c80c85a4ad320cf1fb8aa1c47f96a9ca
kun1225 <kun881225@gmail.com>
2026-02-28 21:02:14 +0800

    chore: scope tailwind prettier sorting to web app
    Move prettier-plugin-tailwindcss from workspace root to apps/web.
    Add app-level prettier config with tailwindFunctions so class sorting also applies inside cva/cn utilities.

--- prettier.config.js
+++ prettier.config.js
@@ -5,8 +5,6 @@ const config = {
   semi: true,
   singleQuote: true,
   trailingComma: 'all',
-  plugins: ['prettier-plugin-tailwindcss'],
-  tailwindFunctions: ['cva', 'cn', 'clsx', 'twMerge'],
 };
 
 export default config;
