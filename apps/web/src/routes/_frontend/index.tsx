ec1f3521d963e41f850b24176718584bf762086f
kun1225 <kun881225@gmail.com>
2026-03-07 20:17:04 +0800

    cleanup(web): remove example components and unused proxy configuration

--- apps/web/src/routes/_frontend/index.tsx
+++ apps/web/src/routes/_frontend/index.tsx
@@ -1,9 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { ComponentExample } from '@/components/component-example';
-
 export const Route = createFileRoute('/_frontend/')({ component: App });
 
 function App() {
-  return <ComponentExample />;
+  return <p>Index Page</p>;
 }
